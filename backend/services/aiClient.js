791ed3c34db484902d59ef71ff5c872d6000851e
curro85 <alexdaw995@gmail.com>
2025-12-09 13:27:26 +0100

    compose to work in local + dockerfiles to create images

--- backend/services/aiClient.js
+++ backend/services/aiClient.js
@@ -2,7 +2,6 @@ const { GoogleGenAI } = require("@google/genai");
 const config = require("./config");
 
 const GEMINI_API_KEY = config.GEMINI_API_KEY;
-console.log(GEMINI_API_KEY);
 
 const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
 
